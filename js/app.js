251b55fa303b8dcf31f7ab009b828654e162a24a
Rayhan138 <rayhankahn138@gmail.com>
2026-07-14 12:47:43 +0600

    Fix deprecated Groq model

--- js/app.js
+++ js/app.js
@@ -193,7 +193,7 @@ function initAIAssistant() {
                         'Authorization': `Bearer ${apiKey}`
                     },
                     body: JSON.stringify({
-                        model: 'llama3-8b-8192',
+                        model: 'llama-3.1-8b-instant',
                         messages: [{ role: 'user', content: fullPrompt }]
                     })
                 });
